d0b3f24880191bf1b8821f544c009d21d4063aa2
Erisvaldo Carvalho <erisvaldodesenvolvedor@gmail.com>
2024-03-08 18:04:03 -0300

    o contêiner docker não está funcionando

--- dockerfile
+++ dockerfile
@@ -2,18 +2,20 @@ FROM python:3.8-slim
 
 # Define o diretório de trabalho como /app
 WORKDIR /app
-ENV FLASK_APP=projeto/app/routes.py
 
-# Copia os arquivos requirements.txt e o Dockerfile para o diretório de trabalho
+# Copia os arquivos requirements.txt para o diretório de trabalho
 COPY requirements.txt ./
 
 # Instala as dependências do Python listadas no requirements.txt
-RUN apt update -y && apt install -y python3 python3-venv python3-pip && \
+RUN apt-get update && apt-get install -y python3-venv python3-pip && \
     python3 -m venv /venv && \
     /venv/bin/pip install --no-cache-dir -r requirements.txt
+
+# Copia o diretório do projeto para o diretório de trabalho
+COPY projeto/ ./projeto/
 
 # Exponha a porta 5000
 EXPOSE 5000
 
 # Define o comando padrão para iniciar o aplicativo Flask
-CMD ["/bin/bash", "-c", "source /venv/bin/activate && python3 app/routes.py"]
+CMD ["bash", "-c", "source /venv/bin/activate && python3 projeto/app/routes.py"]
--- projeto/modules/gestao_de_tarefa/models/tarefa.py
+++ projeto/modules/gestao_de_tarefa/models/tarefa.py
@@ -1,20 +1,27 @@
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
-class Tarefa(db.Model):
-    __tablename__ = 'tarefa'
+# class Tarefa(db.Model):
+#     __tablename__ = 'tarefa'
 
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(100), nullable=False)
-    descricao = db.Column(db.Text, nullable=False)
-    data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
+#     id = db.Column(db.Integer, primary_key=True)
+#     titulo = db.Column(db.String(100), nullable=False)
+#     descricao = db.Column(db.Text, nullable=False)
+#     data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
     
+#     def __init__(self, titulo, descricao):
+#         self.titulo = titulo
+#         self.descricao = descricao
+
+#     def __repr__(self):
+#         return f'<Tarefa {self.id}: {self.titulo}>'
+
+class Tarefa:
     def __init__(self, titulo, descricao):
         self.titulo = titulo
         self.descricao = descricao
 
     def __repr__(self):
-        return f'<Tarefa {self.id}: {self.titulo}>'
-
+        return f"Tarefa(titulo={self.titulo}, descricao={self.descricao})"
